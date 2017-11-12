0efc6dcf6fa15a96b51a26b49857e38041be0d6c
sumtype <james@sumtype.tech>
2017-11-12 02:30:45 -0500

    Added px2rem.py, updated README.md, and updated .gitignore to ignore generated output.txt files.

---
+++ px2rem.py
@@ -0,0 +1,28 @@
+import sys
+
+args = sys.argv
+if len(sys.argv) != 3:
+    print()
+    print('px2rem.py')
+    print('Converts a file with px values (ex: 1px) to rem values (ex: 1rem) based on a specified base font size.')
+    print('Requires two arguments, [file path] and [base font size] to convert.')
+    print()
+    print('Example: $python3 px2em.py [file path] [base font size (float || integer)]')
+    print('Clarification: If [base font size] = 16 and line n of [file path] contains the value "16px" -> line n of [file path] converts to contain the value "1.000rem".')
+    print()
+else:
+    if sys.argv[2].find('px') != -1:
+        sys.argv[2] = sys.argv[2][0 : len(sys.argv[2]) - 2]
+    with open(sys.argv[1]) as file, open('output.txt', 'w') as output:
+        for _ in file:
+            position = _.find('px')
+            if position != -1 and _.find('@media') == -1:
+                backStep = 0
+                while _[position - backStep] != ' ' and _[position - backStep] != ':':
+                    backStep += 1
+                else:
+                    rem = '{0:.3f}'.format(float(_[position - (backStep - 1) : position]) / float(sys.argv[2])) + 'rem'
+                    convertedLine = _.replace(_[position - (backStep - 1) : position + 2], rem)
+                    output.write(convertedLine)
+            else:
+                output.write(_)
